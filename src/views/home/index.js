bd51b0830deab868de2d3c52d16fba5237b4901a
a13821190779 <945039036@qq.com>
2017-11-13 22:45:58 +0800

    Update: swiper垂直方向问题，改用别的拆插件吧，回头自己写一个

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -34,7 +34,6 @@ class Home extends Component {
 		if (this.state.swiper === null) {
 			const swiper = new Swiper('.swiper-container', {
 				direction: 'vertical',
-				height: 'auto',
 				freeMode: true
 			})
 			this.setState({
@@ -44,36 +43,49 @@ class Home extends Component {
 	}
 
 	render () {
+		// 感觉要用hammerjs了。https://github.com/hammerjs/hammer.js
 		const fn = () => {
 			return this.state.viewData.map((el) => {
 				return (
 				// list 自己写，通过MenuValue获取该list的数据
-				<a href={el.MenuValue} key={el.MenuCode}>
-					<div>
+				<div className="content" style={{height: this.contents.offsetWidth / 2 + 'px'}} key={el.MenuCode}>
+					<div className="spacing">
 						<img src={el.ImageUrL || 'http://img4.imgtn.bdimg.com/it/u=2823434616,1362037498&fm=200&gp=0.jpg'} alt=""/>
 						<p>{el.MenuName}</p>
 					</div>
-				</a>)
+				</div>)
 			})
 		}
-		return (
-		<div className={scss.wrap}>
-			<div className="swiper-container">
-			    <div className="swiper-wrapper">
-				    <div className="swiper-slide">
-				    	<div className="swiper-slide">
-				    		<img src="https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=1236117006,3892149680&fm=173&s=0551396CC44EA75556E840920300909B&w=640&h=384&img.JPEG" alt=""/>
-				    	</div>
-			    	    <div className="swiper-slide">
-			    	    	<img src="https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=1236117006,3892149680&fm=173&s=0551396CC44EA75556E840920300909B&w=640&h=384&img.JPEG" alt=""/>
-			    	    </div>
-			    	    <div className="swiper-slide">
-			    	    	{fn()}
-			    	    </div>
-				    </div>
-			    </div>
-			</div>
+		return (<div className={scss.wrap + ' swiper-container'}>
+		    <div className="swiper-wrapper">
+		    	<div className="swiper-slide">
+		    		<img src="https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=1236117006,3892149680&fm=173&s=0551396CC44EA75556E840920300909B&w=640&h=384&img.JPEG" alt=""/>
+		    	</div>
+		        <div className="swiper-slide">
+    	        	<div className="contents" ref={el => this.contents = el}>
+    			        {fn()}
+    	        	</div>
+		        </div>
+		    </div>
 		</div>)
+		// return (
+		// <div className={scss.wrap}>
+		// 	<div className="swiper-container">
+		// 	    <div className="swiper-wrapper">
+		// 		    <div className="swiper-slide">
+		// 		    	<div className="swiper-slide">
+		// 		    		<img src="https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=1236117006,3892149680&fm=173&s=0551396CC44EA75556E840920300909B&w=640&h=384&img.JPEG" alt=""/>
+		// 		    	</div>
+		// 	    	    <div className="swiper-slide">
+		// 	    	    	<img src="https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=1236117006,3892149680&fm=173&s=0551396CC44EA75556E840920300909B&w=640&h=384&img.JPEG" alt=""/>
+		// 	    	    </div>
+		// 	    	    <div className="swiper-slide">
+		// 	    	    	{fn()}
+		// 	    	    </div>
+		// 		    </div>
+		// 	    </div>
+		// 	</div>
+		// </div>)
 	}
 }
 
